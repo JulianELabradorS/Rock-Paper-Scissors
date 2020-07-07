d7a70f7b8312a807cfe6c92c6030f3e61815e4bb
Julian Labrador <lockdownlab@gmail.com>
2020-07-07 11:39:26 -0500

    Updates

---
+++ precache-manifest.d4a763a54901ce960f08dd1fa2110b3d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1dff532a6950160eb5c3dd658cac3ca",
+    "url": "/Rock-Paper-Scissors/index.html"
+  },
+  {
+    "revision": "c9fd8f852e49dd991674",
+    "url": "/Rock-Paper-Scissors/static/css/main.fbfca08b.chunk.css"
+  },
+  {
+    "revision": "6d9794842f13e1a44921",
+    "url": "/Rock-Paper-Scissors/static/js/2.fc859f40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rock-Paper-Scissors/static/js/2.fc859f40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9fd8f852e49dd991674",
+    "url": "/Rock-Paper-Scissors/static/js/main.9cf1fa78.chunk.js"
+  },
+  {
+    "revision": "642d30f4c2c7faf44e53",
+    "url": "/Rock-Paper-Scissors/static/js/runtime-main.13c283cb.js"
+  },
+  {
+    "revision": "aced70522265bc609155bea44802776b",
+    "url": "/Rock-Paper-Scissors/static/media/bg-pentagon.aced7052.svg"
+  },
+  {
+    "revision": "919ab92a5d671f1b7fdd3e5f97eb816c",
+    "url": "/Rock-Paper-Scissors/static/media/icon-close.919ab92a.svg"
+  },
+  {
+    "revision": "7d2e0b18d2860b90407f41297dce461f",
+    "url": "/Rock-Paper-Scissors/static/media/icon-lizard.7d2e0b18.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/Rock-Paper-Scissors/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/Rock-Paper-Scissors/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/Rock-Paper-Scissors/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "b6446e8e1e5d5609deb209d401e779a3",
+    "url": "/Rock-Paper-Scissors/static/media/icon-spock.b6446e8e.svg"
+  },
+  {
+    "revision": "f19a40fbc5296effc230454fa1295890",
+    "url": "/Rock-Paper-Scissors/static/media/image-rules.f19a40fb.svg"
+  },
+  {
+    "revision": "f4c973f743c8b75de9da23335e126dcd",
+    "url": "/Rock-Paper-Scissors/static/media/logo.f4c973f7.svg"
+  }
+]);
